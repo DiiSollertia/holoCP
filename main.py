84f26e173509858ad2543e85d27671cc67af1b18
DiiSollertia <xz.khoo.work@gmail.com>
2022-02-15 13:40:48 +0800

    markdown issues fixed

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ r = praw.Reddit(username = "holoCP",
                 password = os.environ['REDDIT_PW'],
                 client_id = os.environ['REDDIT_ID'],
                 client_secret = os.environ['REDDIT_SECRET'],
-                user_agent = "heroku:holoCP:v17 (by /u/holoCP)")
+                user_agent = "heroku:holoCP:v1 (by /u/holoCP)")
 
 print('setup done')
 
@@ -32,9 +32,11 @@ for comment in subr.stream.comments(skip_existing=True): # this iterates through
         if "!supernenechi" in comment.body:
             comment.reply('''Nene超進化! Super Hyper Ultra Ultimate Deluxe Perfect Amazing Shining God 流派東方不敗 Master Freedom Justice Ginga Victory Prime Strong Cute Beautiful Wonderful Champion Galaxy Baby 最高 勇者王 天元突破 無限 無敵 無双 NENEMAXMAXMAXSTORONG NENECHI!''')
         if "!phoenix" in comment.body:
-            comment.reply('''<h2>To those coming from r/all:</h2>
-Hello! Welcome to the official subreddit for hololive production (wikipedia), a talent agency based out of Tokyo, Japan that manages Virtual YouTubers - think of streamers on Twitch and YouTube, but with virtual avatars instead of a facecam. Most of the talents in Hololive are Japanese, but there are also Indonesian and English branches too! The talents do all sorts of stuff, including but not limited to: gaming streams, karaoke streams, drawing streams and talking streams. Some of the content the talents offer is family friendly, others a bit more risque. The sidebar has links to each talent's Youtube and Twitter accounts.''')
+            comment.reply(
+                '''# To those coming from r/all: <br /> Hello! Welcome to the official subreddit for hololive production (wikipedia), a talent agency based out of Tokyo, Japan that manages Virtual YouTubers - think of streamers on Twitch and YouTube, but with virtual avatars instead of a facecam. Most of the talents in Hololive are Japanese, but there are also Indonesian and English branches too! The talents do all sorts of stuff, including but not limited to: gaming streams, karaoke streams, drawing streams and talking streams. Some of the content the talents offer is family friendly, others a bit more risque. The sidebar has links to each talent's Youtube and Twitter accounts.''')
     except praw.exceptions.RedditAPIException:
         print("API Exception: Probably rate limit")
     except prawcore.exceptions.ResponseException:
         print("Response Exception: Probably HTTP 401 Authentication Error")
+    except:
+        print("Unknown Error")
