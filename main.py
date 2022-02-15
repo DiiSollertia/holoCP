9782b2b9774cb531866021b265f915e7dbe634e5
DiiSollertia <xz.khoo.work@gmail.com>
2022-02-15 13:28:19 +0800

    fixed exceptions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import praw
+import prawcore.exceptions
 import os
 
 #from dotenv import load_dotenv
@@ -11,7 +12,7 @@ r = praw.Reddit(username = "holoCP",
                 password = os.environ['REDDIT_PW'],
                 client_id = os.environ['REDDIT_ID'],
                 client_secret = os.environ['REDDIT_SECRET'],
-                user_agent = "reddit:holoCP:v16 (by /u/holoCP)")
+                user_agent = "heroku:holoCP:v17 (by /u/holoCP)")
 
 print('setup done')
 
@@ -33,7 +34,7 @@ for comment in subr.stream.comments(skip_existing=True): # this iterates through
         if "!phoenix" in comment.body:
             comment.reply('''<h2>To those coming from r/all:</h2>
 Hello! Welcome to the official subreddit for hololive production (wikipedia), a talent agency based out of Tokyo, Japan that manages Virtual YouTubers - think of streamers on Twitch and YouTube, but with virtual avatars instead of a facecam. Most of the talents in Hololive are Japanese, but there are also Indonesian and English branches too! The talents do all sorts of stuff, including but not limited to: gaming streams, karaoke streams, drawing streams and talking streams. Some of the content the talents offer is family friendly, others a bit more risque. The sidebar has links to each talent's Youtube and Twitter accounts.''')
-    except praw.exceptions.APIException:
+    except praw.exceptions.RedditAPIException:
         print("API Exception: Probably rate limit")
-    except praw.exceptions.ResponseException:
+    except prawcore.exceptions.ResponseException:
         print("Response Exception: Probably HTTP 401 Authentication Error")
