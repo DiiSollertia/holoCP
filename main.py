acb420df4e46522b4d177a7388f97a5b952ed609
DiiSollertia <xz.khoo.work@gmail.com>
2022-02-16 14:56:10 +0800

    rebranded to holoquote to avoid abbr.

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ import os
 print('import done')
 
 #setup
-r = praw.Reddit(username = "holoCP",
+r = praw.Reddit(username = "holoquote_bot",
                 password = os.environ['REDDIT_PW'],
                 client_id = os.environ['REDDIT_ID'],
                 client_secret = os.environ['REDDIT_SECRET'],
-                user_agent = "Windows-10:holoCP:v1 (by /u/holoCP)",
+                user_agent = "Windows-10:holoquote_bot:v1 (by /u/holoquote_bot)",
                 ratelimit_seconds = 300,
                 )
 
 print('setup done')
 
-footer = "\n\n"+"---"+"\n\n_I am a copypasta bot. [Documentation here](https://github.com/DiiSollertia/holoCP#readme)_"
+footer = "\n\n"+"---"+"\n\n_I am a copypasta bot. [Documentation here](https://github.com/DiiSollertia/holoquote_bot/tree/main#readme)_"
 
 #responds to mentions
 subr = r.subreddit('testingground4bots') # this chooses a subreddit you want to get comments from
@@ -32,11 +32,13 @@ for comment in subr.stream.comments(skip_existing=True): # this iterates through
         if "!pekopeko" in comment.body:
             comment.reply('''PE↗KO↘PE↗KO↘PE↗KO↘'''+footer)
         if "!guracars" in comment.body:
-            comment.reply('''Hello, ma'am do you have a minute to talk about our lord and savior Lightning McQueen?! Did you know that Lightning McQueen is the star of several feature films such as Cars, Cars 2, Cars 3, Planes: Fire and Rescue, Finding Dory, Toy Story 3, Coco and Ralph breaks the internet? As well as other short film such as Master and the Ghostlight, Miss Fritter's Racing Skoool, Television program such as Cars Toons, Pixar's Popcorn Cars series voiced by none other than Owen Wilson?'''+footer)
+            comment.reply('''[Hello, ma'am do you have a minute to talk about our lord and savior Lightning McQueen?! Did you know that Lightning McQueen is the star of several feature films such as Cars, Cars 2, Cars 3, Planes: Fire and Rescue, Finding Dory, Toy Story 3, Coco and Ralph breaks the internet? As well as other short film such as Master and the Ghostlight, Miss Fritter's Racing Skoool, Television program such as Cars Toons, Pixar's Popcorn Cars series voiced by none other than Owen Wilson?](https://youtu.be/vci-RuRSios?t=6175)'''+footer)
         if "!supernenechi" in comment.body:
             comment.reply('''Nene超進化! Super Hyper Ultra Ultimate Deluxe Perfect Amazing Shining God 流派東方不敗 Master Freedom Justice Ginga Victory Prime Strong Cute Beautiful Wonderful Champion Galaxy Baby 最高 勇者王 天元突破 無限 無敵 無双 NENEMAXMAXMAXSTORONG NENECHI!'''+footer)
         if "!phoenix" in comment.body:
             comment.reply('''# To those coming from r/all:  '''+'''\n\nHello! Welcome to the official subreddit for hololive production (wikipedia), a talent agency based out of Tokyo, Japan that manages Virtual YouTubers - think of streamers on Twitch and YouTube, but with virtual avatars instead of a facecam. Most of the talents in Hololive are Japanese, but there are also Indonesian and English branches too! The talents do all sorts of stuff, including but not limited to: gaming streams, karaoke streams, drawing streams and talking streams. Some of the content the talents offer is family friendly, others a bit more risque. The sidebar has links to each talent's Youtube and Twitter accounts.'''+footer)
+        if "!faq" in comment.body:
+            comment.reply('''[FAQ IS LOVE!](https://youtu.be/T142Djb4Jbw?t=5982)''')
     except praw.exceptions.RedditAPIException:
         print("API Exception: Probably rate limit")
     except prawcore.exceptions.ResponseException:
